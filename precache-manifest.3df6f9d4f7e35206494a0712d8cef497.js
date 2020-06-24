19c6102641e7ed360667e89d76e52aba5c51b30e
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-06-24 13:39:27 -0400

    Updates

---
+++ precache-manifest.3df6f9d4f7e35206494a0712d8cef497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e9837503de2ef71c8a6928afec4ab",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "7149437285f67d0f6c90",
+    "url": "/random-quote-machine/static/css/main.8b75cdfa.chunk.css"
+  },
+  {
+    "revision": "342a060a4da3acdbfd2a",
+    "url": "/random-quote-machine/static/js/2.7634dfb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.7634dfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7149437285f67d0f6c90",
+    "url": "/random-quote-machine/static/js/main.eb2a3f33.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
